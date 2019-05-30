93aa0fc54d87f59c389f126ce0441613c8906cb2
Fernando Valle <fernando@jornadatecnologia.com>
2019-05-29 21:50:06 -0300

    first commit with file

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-toolbar
+      style="background-color: #993399"
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-btn icon dark>
+        <v-icon>keyboard_arrow_left</v-icon>
+      </v-btn>
+      <v-toolbar-title class="white--text">Criar Treino</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-layout justify-center align-center>
+    <v-flex>
+      <v-flex xs12>
+        <v-text-field
+          outline
+          label="Digite o nome do exercício"
+          prepend-inner-icon="search"
+          append-icon="short_text"
+          height="3px"
+        />
+      </v-flex>
+      <v-flex xs12 style="margin-top:-20px">
+        <v-layout wrap>
+          <v-flex xs7 style="margin-right:5px; margin-start:8px">
+            <v-flex xs12>
+              <v-card>
+                <v-card-title class="caption primary--text">
+                  Supino Reto Inclinado
+                </v-card-title>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 style="padding-top:5px">
+              <v-card>
+                <v-card-title class="caption primary--text">
+                  Supino Reto Inclinado
+                </v-card-title>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 style="padding-top:5px">
+              <v-card>
+                <v-card-title class="caption primary--text">
+                  Supino Reto Inclinado
+                </v-card-title>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 style="padding-top:5px">
+              <v-card>
+                <v-card-title class="caption primary--text">
+                  Supino Reto Inclinado
+                </v-card-title>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 style="padding-top:5px">
+              <v-card>
+                <v-card-title class="caption primary--text">
+                  Supino Reto Inclinado
+                </v-card-title>
+              </v-card>
+            </v-flex>
+          </v-flex>
+          <v-flex d-flex xs4>
+            <v-layout wrap>
+              <v-flex xs12 d-flex style="height:50%">
+                <v-card>
+                  <v-card-title
+                    class="display-1 text-md-center primary--text"
+                    style="padding-left:37%"
+                  >
+                    A
+                  </v-card-title>
+                  <v-card-text class="caption primary--text">
+                    Selecione 5 exercícios
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex xs12 d-flex style="height:30%; padding-top:5px">
+                <v-card>
+                  <v-card-title
+                    class="display-1 text-md-center primary--text"
+                    style="padding-left:37%"
+                  >
+                    B
+                  </v-card-title>
+                </v-card>
+              </v-flex>
+              <v-flex xs12 d-flex style="height:30%; padding-top:5px">
+                <v-card>
+                  <v-card-title
+                    class="display-1 text-md-center primary--text"
+                    style="padding-left:37%"
+                  >
+                    C
+                  </v-card-title>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
